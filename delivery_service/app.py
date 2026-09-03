320a726ae4c1554cf5beb14c14160d8b0546ec2a
turococ <84099957+turococ@users.noreply.github.com>
2026-09-04 02:23:39 +0300

    Update app.py

--- delivery_service/app.py
+++ delivery_service/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 app.secret_key = 'super-secret-key'
 
 # Настройки админа
-ADMIN_USERNAME = ""
-ADMIN_PASSWORD = ""
+ADMIN_USERNAME = "YOUR USERNAME"
+ADMIN_PASSWORD = "YOUR PASSWORD"
 
 # Подключение к БД
 def get_db_connection():
